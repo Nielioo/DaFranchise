03432478ae8597d47410598714adb9f76de139d1
Daniel Aprillio <danielaprillio02@gmail.com>
2021-11-04 16:45:46 +0700

    add footer

---
+++ DaFranchise/resources/views/navigation/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="bg-light text-center text-lg-start @yield('footer')">
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2021 Copyright: Daniel Aprillio
+    </div>
+</footer>
