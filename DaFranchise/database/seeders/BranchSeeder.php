59298ff7f7b72625c31f641456e21ebbd437957a
Daniel Aprillio <danielaprillio02@gmail.com>
2021-11-04 12:30:02 +0700

    add seeder data

--- DaFranchise/database/seeders/BranchSeeder.php
+++ DaFranchise/database/seeders/BranchSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class BranchSeeder extends Seeder
 {
@@ -13,6 +14,37 @@ class BranchSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('branches')->insert([
+            'branch_id' => '1',
+			'branch_location' => 'Supermal Pakuwon Indah, Lantai 1, Jl. Mayjen Yono Suwoyo No. 2, Surabaya, Indonesia',
+			'branch_phone' => '+62317390075',
+			'branch_rating' => '4.6',
+			'franchise_id' => '1',
+
+			'created_at' => \Carbon\Carbon::now(),
+			'updated_at' => \Carbon\Carbon::now(),
+		]);
+
+        DB::table('branches')->insert([
+            'branch_id' => '2',
+			'branch_location' => 'Jl. HR Muhammad No. 102, Surabaya, Indonesia',
+			'branch_phone' => '+623199147388',
+			'branch_rating' => '4.8',
+			'franchise_id' => '1',
+
+			'created_at' => \Carbon\Carbon::now(),
+			'updated_at' => \Carbon\Carbon::now(),
+		]);
+
+        DB::table('branches')->insert([
+            'branch_id' => '3',
+			'branch_location' => 'Pakuwon Mall, Jl. Mayjen Yono Suwoyo No.2, Surabaya, Indonesia',
+			'branch_phone' => '+6281216672320',
+			'branch_rating' => '4.5',
+			'franchise_id' => '2',
+
+			'created_at' => \Carbon\Carbon::now(),
+			'updated_at' => \Carbon\Carbon::now(),
+		]);
     }
 }
--- DaFranchise/database/seeders/FranchiseSeeder.php
+++ DaFranchise/database/seeders/FranchiseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class FranchiseSeeder extends Seeder
 {
@@ -13,6 +14,33 @@ class FranchiseSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('franchises')->insert([
+			'franchise_id' => '1',
+			'franchise_name' => 'Excelso',
+			'franchise_founded' => '1991',
+			'franchise_type' => 'Food and Beverage',
+			'franchise_outlet' => '126',
+			'franchise_investment' => '250 juta s/d 1.5 milyar',
+			'franchise_website' => 'https://excelso-coffee.com',
+			'franchise_description' => 'Excelso adalah merek kopi dan kafe asli dari Indonesia yang merupakan bagian dari Kapal Api Grup, produsen kopi terbesar di Indonesia. Selain tersedia kopi kemasan premium, Excelso juga memiliki kafe yang pertama kali dibuka pada bulan September 1991 di Plaza Indonesia, Jakarta. Excelso telah berkembang menjadi salah satu kafe yang memiliki reputasi di Indonesia, hingga kini telah memiliki jaringan lebih dari 100 outlet dan hadir di lebih dari 30 kota besar di Indonesia.',
+
+			'created_at' => \Carbon\Carbon::now(),
+			'updated_at' => \Carbon\Carbon::now(),
+		]);
+
+        DB::table('franchises')->insert([
+			'franchise_id' => '2',
+			'franchise_name' => 'Lotte Grosir',
+			'franchise_founded' => '1993',
+			'franchise_type' => 'Retail',
+			'franchise_outlet' => '47',
+			'franchise_investment' => '77 juta',
+			'franchise_website' => 'https://www.lottemart.co.id/',
+			'franchise_description' => 'Perusahaan ritel kenamaan Korea Selatan, LOTTE Grosir dan LOTTE Mart, yang merupakan bagian dari LOTTE GROUP, perusahaan Korea pertama yang melakukan penetrasi pasar dalam industri wholesale dan ritel di Indonesia.',
+
+			'created_at' => \Carbon\Carbon::now(),
+			'updated_at' => \Carbon\Carbon::now(),
+		]);
+
     }
 }
