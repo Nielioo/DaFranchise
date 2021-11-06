2a69f460dd5d5835d8d941c33ba81361f182e178
Daniel Aprillio <danielaprillio02@gmail.com>
2021-11-06 21:05:22 +0700

    fix branch rating

--- DaFranchise/resources/views/content/Branch/branchShow.blade.php
+++ DaFranchise/resources/views/content/Branch/branchShow.blade.php
@@ -31,7 +31,7 @@
             <h4 class="h4 mt-4 mb-3">Details</h4>
             <p class="text-paragraph"><b>Location: </b>{{ $branch['branch_location'] }}</p>
             <p class="text-paragraph"><b>Phone Number: </b>{{ $branch['branch_phone'] }}</p>
-            <p class="text-paragraph"><b>Rating: </b>{{ $branch['branch_location'] }}/5</p>
+            <p class="text-paragraph"><b>Rating: </b>{{ $branch['branch_rating'] }}/5</p>
             <p class="text-paragraph"><b>Franchise: </b>{{ $branch->franchise->franchise_name }}</p>
 
         </div>
